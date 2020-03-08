0df5455b0c83f953dfa4eafc822d33c305a52054
Tran Phuc Vinh <vinhtranphuc@gmail.com>
2020-03-08 21:26:01 +0700

    add img tutorial

--- src/main/java/com/tranphucvinh/controller/UserController.java
+++ src/main/java/com/tranphucvinh/controller/UserController.java
@@ -22,7 +22,7 @@ import com.tranphucvinh.security.UserPrincipal;
 
 @RestController
 @RequestMapping("/api/users")
-@CrossOrigin(origins = {"http://localhost:3000","http://localhost:3001"})
+//@CrossOrigin(origins = {"http://localhost:3000","http://localhost:3001"})
 public class UserController {
 
     @Autowired
